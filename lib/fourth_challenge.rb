1287ad3364711baa4768c54929642382276f7815
Alex Sterrantino <alexsterrantino@gmail.com>
2020-02-04 23:26:47 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,16 +5,16 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        benvolio => {:name => "Benvolio",:age => "17",:attitude => "worried"},
-        mercutio => {:name => "Mercutio",:age => "18",:attitude => "hot-headed"}]
+        "Benvolio" => {:name => "Benvolio",:age => "17",:attitude => "worried"},
+        "Mercutio" => {:name => "Mercutio",:age => "18",:attitude => "hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        steven = {:name => "Steven",:age => "30",:attitude => "confused"},
-        nurse = {:name => "Nurse",:age => "44",:attitude => "worried"}
+        "Steven" => {:name => "Steven",:age => "30",:attitude => "confused"},
+        "Nurse" => {:name => "Nurse",:age => "44",:attitude => "worried"}
       ]
    }
 }
